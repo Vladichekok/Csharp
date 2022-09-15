853700ed9e3c97acdccbf013e751d68c0c03430f
Vladichekok <lekontrop321@gmail.com>
2022-09-15 21:24:57 +0300

    lab3

---
+++ lab3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x;
+            double y;
+            Console.WriteLine("Введiть х");
+            x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введiть y");
+            y = double.Parse(Console.ReadLine());
+            if (x >= 0 & y >=0 | x <=0 & y<=0)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+        }
+    }
+}
